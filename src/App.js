72c39c4001fcddad31ef250a79867c1b0ae770ea
Kyle Wheeless <kyle@wheeless.dev>
2021-11-30 14:51:53 -0800

    add all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { HiCode, HiExternalLink } from 'react-icons/hi';
+// import logo from './logo.svg';
+// import { HiCode, HiExternalLink } from 'react-icons/hi';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
@@ -10,6 +10,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -22,6 +23,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <input
+          type='search'
+          placeholder='Search Monsters'
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
